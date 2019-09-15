bd203fce30863f0b2599f0d5164f255171ffbc16
saul365 <saul_bh365@comunidad.unam.mx>
2019-09-14 23:55:02 -0500

    i2c fuly functional

--- test/i2c.cpp
+++ test/i2c.cpp
@@ -6,15 +6,17 @@ int main(){
 	int devId=0x68;
 	
 	int fd=wiringPiI2CSetup (devId);
+
+	//printf("%d %d %d %d %d %d",minutos,horas,dia,date,mes,years);
 	
 	wiringPiI2CWriteReg8 (fd, 0x00, 0x00) ;
-	wiringPiI2CWriteReg8 (fd, 0x01, 0x10) ;
-	wiringPiI2CWriteReg8 (fd, 0x02, 0x03) ;
+	wiringPiI2CWriteReg8 (fd, 0x01, 0x35) ;
+	wiringPiI2CWriteReg8 (fd, 0x02, 0x22) ;
 	wiringPiI2CWriteReg8 (fd, 0x03, 0x06) ;
 	wiringPiI2CWriteReg8 (fd, 0x04, 0x14) ;
 	wiringPiI2CWriteReg8 (fd, 0x05, 0x09) ;
 	wiringPiI2CWriteReg8 (fd, 0x06, 0x19) ;
-	wiringPiI2CWriteReg8 (fd, 0x00, 0x10) ;
+	wiringPiI2CWriteReg8 (fd, 0x07, 0x10) ;
 
 	return 0;
 	
--- test/readI2C.cpp
+++ test/readI2C.cpp
@@ -3,14 +3,15 @@
 #include <string.h>
 
 
-char dias[][]={{"Lunes"},{"Martes"},{"Miercoles"},{"Jueves"},{"Viernes"},{"Sabado"},{"Domingo"}};
+//char dias[][]={{"Lunes"},{"Martes"},{"Miercoles"},{"Jueves"},{"Viernes"},{"Sabado"},{"Domingo"}};
 
+int convert(int num);
 
 int main(){
 	int devId=0x68;
+	int temp=0;
 	
 	int fd=wiringPiI2CSetup (devId);
-	
 	int secs= wiringPiI2CReadReg8 (fd, 0x00);
 	int mins= wiringPiI2CReadReg8 (fd, 0x01);
 	int hrs= wiringPiI2CReadReg8 (fd, 0x02);
@@ -18,8 +19,17 @@ int main(){
 	int date= wiringPiI2CReadReg8 (fd, 0x04);
 	int mes= wiringPiI2CReadReg8 (fd, 0x05);
 	int year= wiringPiI2CReadReg8 (fd, 0x06);
-
 	
 	
-	printf("Son las %d:%d:%d del %s %d,%d,%d",hrs,mins,secs,dias[day],date,mes,year);
+	printf("Son las %d:%d:%d del %d %d/%d/%d\n",convert(hrs),convert(mins),convert(secs),day,convert(date),convert(mes),convert(year));
+}
+int convert(int num){
+	printf("%d\n",num);
+	int dec=num;
+	int tmp=dec>>4;
+	int unit= num-(tmp<<4);
+	dec=tmp*10;
+	tmp=dec+unit;
+	
+	return tmp;
 }
